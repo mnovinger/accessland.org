f112467b58203551df5104040d64b0f8b6455e8b
Richard Littauer <richard.littauer@gmail.com>
2014-10-27 01:03:17 -0700

    Removed EFF code from JS file, reset counts with new URL, closes #7

--- js/grayscale.js
+++ js/grayscale.js
@@ -23,16 +23,16 @@ $(function() {
 /* ==========================================================================
    Social counts
    ==========================================================================*/
-var shareUrl = 'https://shutthebackdoor.net';
+var shareUrl = 'https://accessland.org';
 $.ajax('https://d28jjwuneuxo3n.cloudfront.net/?networks=facebook,twitter,googleplus&url=' + shareUrl, {
     success: function(res, err) {
         $.each(res, function(network, value) {
             var count = value;
             if (count / 10000 > 1) {
-                count = Math.ceil(count / 1000) + 'k'
+                count = Math.ceil(count / 1000) + 'k';
             }
             $('[data-network="' + network + '"]').attr('count', count);
-        })
+        });
     },
     dataType: 'jsonp',
     cache         : true,
@@ -60,7 +60,7 @@ if (!Object.keys) {
         for (p in o)
             if (Object.prototype.hasOwnProperty.call(o, p)) k.push(p);
         return k;
-    }
+    };
 }
 
 (function() {
@@ -69,99 +69,99 @@ if (!Object.keys) {
 
 
 })();
-$(document).ready(function(){
+// $(document).ready(function(){
 
-  var referalMap = {
-      'fftf': {
-          name: 'Fight for the Future',
-          policy: 'http://www.fightforthefuture.org/privacy/'
-      },
-      'eff': {
-          name: 'EFF',
-          policy: 'https://www.eff.org/policy'
-      },
-      'dp': {
-          name: 'Demand Progress',
-          policy: 'http://www.demandprogress.org/privacy/'
-      },
-      'fp': {
-          name: 'Free Press',
-          policy: 'http://www.freepress.net/privacy-copyright'
-      }
-  };
-  var referalKeys = Object.keys(referalMap);
-  var referalParam = getParameterByName('r');
-  var referalOrg;
-  var slug;
+//   var referalMap = {
+//       // 'fftf': {
+//       //     name: 'Fight for the Future',
+//       //     policy: 'http://www.fightforthefuture.org/privacy/'
+//       // },
+//       // 'eff': {
+//       //     name: 'EFF',
+//       //     policy: 'https://www.eff.org/policy'
+//       // },
+//       // 'dp': {
+//       //     name: 'Demand Progress',
+//       //     policy: 'http://www.demandprogress.org/privacy/'
+//       // },
+//       // 'fp': {
+//       //     name: 'Free Press',
+//       //     policy: 'http://www.freepress.net/privacy-copyright'
+//       // }
+//   };
+//   var referalKeys = Object.keys(referalMap);
+//   var referalParam = getParameterByName('r');
+//   var referalOrg;
+//   var slug;
 
-  // Allows a page to have a selected org always
-  if(typeof alwaysSelected !== 'undefined') {
-      referalParam = alwaysSelected;
-  }
+//   // Allows a page to have a selected org always
+//   if(typeof alwaysSelected !== 'undefined') {
+//       referalParam = alwaysSelected;
+//   }
 
-  if (referalParam in referalMap) {
-    referalOrg = referalMap[referalParam];
-    slug = referalParam;
-  } else {
-    var randomOrgIndex = Math.floor(Math.random() * referalKeys.length);
-    referalOrg = referalMap[referalKeys[randomOrgIndex]];
-    slug = referalKeys[randomOrgIndex];
-  }
-  $('.org-name').text(referalOrg.name);
-  $('.org-slug').val(slug);
-  $('.org-privacy').attr('href', referalOrg.policy);
-  if(slug === 'eff') {
-    $('#subscriber-checkbox').removeAttr('checked');
-  }
+//   if (referalParam in referalMap) {
+//     referalOrg = referalMap[referalParam];
+//     slug = referalParam;
+//   } else {
+//     var randomOrgIndex = Math.floor(Math.random() * referalKeys.length);
+//     referalOrg = referalMap[referalKeys[randomOrgIndex]];
+//     slug = referalKeys[randomOrgIndex];
+//   }
+//   $('.org-name').text(referalOrg.name);
+//   $('.org-slug').val(slug);
+//   $('.org-privacy').attr('href', referalOrg.policy);
+//   if(slug === 'eff') {
+//     $('#subscriber-checkbox').removeAttr('checked');
+//   }
 
-  $.fn.serializeObject = function() {
-   var o = {};
-   var a = this.serializeArray();
-   $.each(a, function() {
-       if (o[this.name]) {
-           if (!o[this.name].push) {
-               o[this.name] = [o[this.name]];
-           }
-           o[this.name].push(this.value || '');
-       } else {
-           o[this.name] = this.value || '';
-       }
-   });
-   return o;
- };
-   var $form = $('.email-signup');
-    var emailLogged = false;
-    var logDataFallback = function (data) {
-      console.log(data);
-      if(!emailLogged) {
-        emailLogged = true;
-        var dbData = {
-          email: data.data.email,
-          org: slug
-        };
-        $.ajax('https://email-congress.herokuapp.com/shutthebackdoor', {
-          data: dbData,
-          method: 'POST',
-          success: function(response){
-          },
-          dataType: 'json'
-        });
-      }
-    }
-   $form.submit(function(){
+//   $.fn.serializeObject = function() {
+//    var o = {};
+//    var a = this.serializeArray();
+//    $.each(a, function() {
+//        if (o[this.name]) {
+//            if (!o[this.name].push) {
+//                o[this.name] = [o[this.name]];
+//            }
+//            o[this.name].push(this.value || '');
+//        } else {
+//            o[this.name] = this.value || '';
+//        }
+//    });
+//    return o;
+//  };
+//    var $form = $('.email-signup');
+//     var emailLogged = false;
+//     var logDataFallback = function (data) {
+//       console.log(data);
+//       if(!emailLogged) {
+//         emailLogged = true;
+//         var dbData = {
+//           email: data.data.email,
+//           org: slug
+//         };
+//         $.ajax('https://email-congress.herokuapp.com/shutthebackdoor', {
+//           data: dbData,
+//           method: 'POST',
+//           success: function(response){
+//           },
+//           dataType: 'json'
+//         });
+//       }
+//     }
+//    $form.submit(function(){
 
-     // Because there is no server side error handling, we can just assume success anyway
+//      // Because there is no server side error handling, we can just assume success anyway
 
 
-            logDataFallback({data: $form.serializeObject()});
-  $form.html('Thank you for signing up!')
-//$('#thank-you-message').append("<p> Thanks for signing up! </p>");
-//$form.remove();
-//$('.disclaimer').remove();
-      return false;
-   });
+//             logDataFallback({data: $form.serializeObject()});
+//   $form.html('Thank you for signing up!')
+// //$('#thank-you-message').append("<p> Thanks for signing up! </p>");
+// //$form.remove();
+// //$('.disclaimer').remove();
+//       return false;
+//    });
 
-});
+// });
 
 
 
@@ -173,14 +173,14 @@ $( ".fblinkthis" ).click(function() {
     var url = $(this).attr("href");
     window.open(url, "Share on Facebook", "width=650,height=500");
     return false;
-})
+});
 $( ".twlinkthis" ).click(function() {
     var url = $(this).attr("href");
     window.open(url,"Twitter","width=550,height=420");
     return false;
-})
+});
 $( ".gpluslinkthis" ).click(function() {
     var url = $(this).attr("href");
     window.open(url,"Share on Google Plus","width=500,height=436");
     return false;
-})
+});
